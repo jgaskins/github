79857551e5d2165f41a62176ad40b86925bc45fd
Jamie Gaskins <jgaskins@hey.com>
2025-03-29 15:54:05 -0500

    Remove unnecessary `*_url` methods from Account
    These are supposed to be hypermedia-like URLs. We don't need them since
    this client isn't designed around hypermedia-esque JSON.

--- src/account.cr
+++ src/account.cr
@@ -13,15 +13,6 @@ module GitHub
     getter gravatar_id : String
     getter url : URI
     getter html_url : URI
-    getter followers_url : URI
-    getter following_url : URI
-    getter gists_url : URI
-    getter starred_url : URI
-    getter subscriptions_url : URI
-    getter organizations_url : URI
-    getter repos_url : URI
-    getter events_url : URI
-    getter received_events_url : URI
     getter type : String
     getter? site_admin : Bool
   end
