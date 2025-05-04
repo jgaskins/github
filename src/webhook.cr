fe777771cc44693ef5709ce1c1b6bbf167079a00
Jamie Gaskins <jgaskins@hey.com>
2025-05-04 18:06:42 -0500

    Installation target type/id aren't guaranteed

--- src/webhook.cr
+++ src/webhook.cr
@@ -6,8 +6,8 @@ module GitHub
     getter id : Int64
     getter delivery : String
     getter type : String
-    getter installation_target_type : String
-    getter installation_target_id : Int64
+    getter installation_target_type : String?
+    getter installation_target_id : Int64?
     getter event : Event
 
     def initialize(@id, @delivery, @type, @installation_target_type, @installation_target_id, @event)
